1d2fa5f9872d7cf5fa10df18297caba2212e4e09
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-06-27 19:37:31 -0600

    Initial commit: start.spring.io

---
+++ src/main/kotlin/org/conservationco/kotlinoauth2clientstarter/KotlinOauth2ClientStarterApplication.kt
@@ -0,0 +1,11 @@
+package org.conservationco.kotlinoauth2clientstarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinOauth2ClientStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinOauth2ClientStarterApplication>(*args)
+}
---
+++ src/test/kotlin/org/conservationco/kotlinoauth2clientstarter/KotlinOauth2ClientStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.conservationco.kotlinoauth2clientstarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinOauth2ClientStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
